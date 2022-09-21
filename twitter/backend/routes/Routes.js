f41267214714d06334ddc9a16d59662d3ca52b66
Priya Gupta <37833111+Priygupt13@users.noreply.github.com>
2022-09-21 16:45:23 -0700

    Update Routes.js

--- twitter/backend/routes/Routes.js
+++ twitter/backend/routes/Routes.js
@@ -1,5 +1,5 @@
 /* 
- This file has get functionality to fetch all the tweet of a user from twitter account
+ This file has get and search functionality to fetch all the tweet of a user from twitter account
  First we need to pass the username in our case it's maveric1303 to get all tweets.
  @param username
  output list of tweets
